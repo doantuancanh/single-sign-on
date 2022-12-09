5add887e4898fceb1483100401d952178b5821e0
canhdt <canhdt@teky.vn>
2022-12-09 11:22:23 +0700

    update allow host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.hosts = [
+    IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
+    IPAddr.new("::/0"),             # All IPv6 addresses.
+    "localhost",                    # The localhost reserved domain.
+    ENV["RAILS_DEVELOPMENT_HOSTS"]  # Additional comma-separated hosts for development.
+  ]
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
